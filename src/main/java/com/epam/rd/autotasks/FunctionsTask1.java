139fdc477859d5b4a46afac80f6bb4c8c7c7908f
mmveres <mmveres@gmail.com>
2022-04-19 14:25:25 +0300

    function-task1-master

---
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask1.java
@@ -0,0 +1,20 @@
+package com.epam.rd.autotasks;
+
+
+class FunctionsTask1 {
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static boolean isSorted(int[] array, SortOrder order) {
+       //TODO: Delete line below and write your own solution
+        throw new UnsupportedOperationException();
+    }
+
+
+
+
+}
