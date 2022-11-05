af05e6b128c5d1699e39a11b1fafa25b25dd0a5a
Vitalii Kalinchyk <karlkory@gmail.com>
2022-11-05 19:58:27 +0200

    5 11

--- src/main/java/com/epam/rd/autotasks/FunctionsTask1.java
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask1.java
@@ -1,6 +1,9 @@
 package com.epam.rd.autotasks;
 
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 class FunctionsTask1 {
     /**
      * <summary>
@@ -10,11 +13,16 @@ class FunctionsTask1 {
      * IllegalArgumentException
      */
     public static boolean isSorted(int[] array, SortOrder order) {
-       //TODO: Delete line below and write your own solution
-        throw new UnsupportedOperationException();
+        if (array == null || array.length == 0 || order == null) {
+            throw new IllegalArgumentException();
+        }
+        Comparator<Integer> comparator = null;
+        if (order == SortOrder.ASC) {
+            comparator = Comparator.naturalOrder();
+        } else if (order == SortOrder.DESC) {
+            comparator = Comparator.reverseOrder();
+        }
+        int[] ints = Arrays.stream(array).boxed().sorted(comparator).mapToInt(Integer::valueOf).toArray();
+        return Arrays.equals(array, ints);
     }
-
-
-
-
 }
